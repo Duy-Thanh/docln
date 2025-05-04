6f36354078a4fe5049044e36c1c62ddd5d402231
nekkochan0x0007 <nekkochan0x0007@tutamail.com>
2025-05-04 22:31:40 +0700

    [release] Bump version to 2025.05.04

--- lib/screens/SettingsScreen.dart
+++ lib/screens/SettingsScreen.dart
@@ -69,7 +69,7 @@ class SettingsScreenState extends State<SettingsScreen>
     with SingleTickerProviderStateMixin {
   final SettingsService _settingsService = SettingsService();
   final CrawlerService _crawlerService = CrawlerService();
-  static const String _appVersion = 'Version: 2025.05.03';
+  static const String _appVersion = 'Version: 2025.05.04';
   bool isDarkMode = false;
   String? currentServer;
   double textSize = 16.0;
