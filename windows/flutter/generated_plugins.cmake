66377d590142a08a907a0a0ec12e0314bd48e150
Nekkochan0x0007 <nekkochan0x0007@tutamail.com>
2025-10-10 07:00:13 +0700

    Remove unused libraries

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -7,7 +7,6 @@ list(APPEND FLUTTER_PLUGIN_LIST
   file_selector_windows
   flutter_secure_storage_windows
   permission_handler_windows
-  sentry_flutter
   share_plus
   sqlcipher_flutter_libs
   url_launcher_windows
@@ -16,7 +15,6 @@ list(APPEND FLUTTER_PLUGIN_LIST
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
   flutter_local_notifications_windows
-  jni
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
